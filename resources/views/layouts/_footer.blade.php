35393fe60b91a2e242b5c89162d5198387b43eff
vagrant <vagrant@homestead>
2019-04-30 09:35:45 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="https://qingye.art" target="_blank">Qingye</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:qingye1020@gmail.com">联系我们</a></p>
+  </div>
+</footer>
