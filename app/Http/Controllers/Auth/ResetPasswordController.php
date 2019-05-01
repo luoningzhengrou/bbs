b66b04e4c4a0e82f0a20f5c85a3e89f516719845
vagrant <vagrant@homestead>
2019-05-01 07:56:24 +0000

    增加重置密码成功提示

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -36,5 +37,11 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    protected function sendResetResponse(Request $request, $response)
+    {
+        session()->flush('success', '密码更新成功，您已成功登陆！');
+        return redirect($this->redirectPath());
     }
 }
